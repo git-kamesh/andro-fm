297c906b2328a0172552f7b81129e876fd664128
kamesh sethupathi <kamesh.sethupathi@testsigma.com>
2025-02-15 16:33:18 +0530

    feat: added AlbumCard component

---
+++ src/components/AlbumCard.tsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames';
+
+interface AlbumCardProps {
+  coverImage: string;
+  albumName: string;
+  albumUrl: string;
+  artistName: string;
+}
+
+export const AlbumCard: React.FC<AlbumCardProps> = ({ coverImage, albumName, albumUrl, artistName }) => {
+  return (
+    <a
+      className={classNames(
+        'flex flex-col hover:shadow-sm p-2 transition-all duration-200',
+        'rounded border border-transparent hover:border-slate-200'
+      )}
+      href={albumUrl} target='_blank'
+    >
+      <img src={coverImage} className='w-full' />
+      <div className='text-base text-blue-600 font-semibold mt-2'>{albumName}</div>
+      <div className='text-sm text-slate-600 font-medium'>{artistName}</div>
+    </a>
+  );
+}
